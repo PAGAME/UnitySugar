bd69e48bf41a42cc918aa3eac223e4abaaf8d56c
ForgiveMyAwfulEngilsh <471073043@qq.com>
2015-07-09 15:09:10 +0800

    auto compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp'),
-  uglify = require('gulp-uglify');
+  uglify = require('gulp-uglify'),
+  coffee = require('gulp-coffee');
 
-gulp.task('default', function(){
-  gulp.src('ls.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/'));
+gulp.task('test', function(){
+	gulp.src('ls.js')
+		.pipe(uglify)
+		.pipe(gulp.dest('dist/'));
 });
+
+gulp.task('function', function(){
+	gulp.src('functions.coffee')
+		.pipe(coffee())
+		.pipe(gulp.dest('dist/'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+	gulp.watch('functions.coffee', ['function']);
+  // gulp.watch(paths.scripts, ['scripts']);
+  // gulp.watch(paths.images, ['images']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['test', 'function', 'watch']);
--- import_gulp.bat
+++ import_gulp.bat
@@ -1,1 +1,2 @@
 npm install --save-dev gulp gulp-uglify
+npm install --save-dev gulp gulp-livereload
